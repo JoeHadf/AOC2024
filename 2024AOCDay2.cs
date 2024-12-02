6ff775cadaae54e1704006940efe81899b4adcf3
JoeHadf <151024605+JoeHadf@users.noreply.github.com>
2024-12-02 21:56:21 +0000

    Add files via upload

--- 2024AOCDay2.cs
+++ 2024AOCDay2.cs
@@ -1,64 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Net;
-using System.Xml;
 
-namespace Day1
+namespace Day2
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            string[] input = File.ReadAllLines(@"C:\Users\Josep\Documents\AOC2024\Day1Input.txt");
-            char[] seperators = new char[] { ' ' };
+            string[] input = File.ReadAllLines(@"C:\Users\Josep\Documents\AOC2024\Day2Input.txt");
+            char[] separators = new char[] { ' ' };
 
-            Dictionary<int, int> valueQuantityInList1 = new Dictionary<int, int>(input.Length);
-            Dictionary<int, int> valueQuantityInList2 = new Dictionary<int, int>(input.Length);
+            List<List<int>> levelLists = new List<List<int>>(input.Length);
 
-            HashSet<int> list1Values = new HashSet<int>(input.Length);
-            
             for (int i = 0; i < input.Length; i++)
             {
-                string[] splitInput = input[i].Split(seperators, System.StringSplitOptions.RemoveEmptyEntries);
-
-                int list1Value = int.Parse(splitInput[0]);
-                int list2Value = int.Parse(splitInput[1]);
-
-                list1Values.Add(list1Value);
-
-                if (valueQuantityInList1.ContainsKey(list1Value))
+                string[] splitInput = input[i].Split(separators, StringSplitOptions.RemoveEmptyEntries);
+                List<int> currentLevelList = new List<int>(splitInput.Length);
+                for (int j = 0; j < splitInput.Length; j++)
                 {
-                    valueQuantityInList1[list1Value] += 1;
+                    currentLevelList.Add(int.Parse(splitInput[j]));
                 }
-                else
-                {
-                    valueQuantityInList1[list1Value] = 1;
-                }
-                
-                if (valueQuantityInList2.ContainsKey(list2Value))
-                {
-                    valueQuantityInList2[list2Value] += 1;
-                }
-                else
-                {
-                    valueQuantityInList2[list2Value] = 1;
-                }
-                
+                levelLists.Add(currentLevelList);
             }
 
-            long similaritySum = 0;
+            int safeCount = 0;
 
-            foreach ( (int key, int value) in valueQuantityInList1)
+            for (int i = 0; i < input.Length; i++)
             {
-                int occurrencesInList1 = idAndQuantityPair.Value;
-                int occurrencesInList2 = valueQuantityInList2.TryGetValue(idAndQuantityPair.Key, out int occurrences) ? occurrences : 0;
+                List<int> currentLevelList = levelLists[i];
 
-                similaritySum += occurrencesInList1 * idAndQuantityPair.Key * occurrencesInList2;
+                bool listIsSafe = LevelsAreSafe(currentLevelList, out int initialBadLevelIndex);
+
+                if (!listIsSafe)
+                {
+                    List<int> removalList0 = new List<int>(currentLevelList);
+                    List<int> removalList1 = new List<int>(currentLevelList);
+                    List<int> removalList2 = new List<int>(currentLevelList);
+                    
+                    removalList0.RemoveAt(0);
+                    removalList1.RemoveAt(initialBadLevelIndex);
+                    removalList2.RemoveAt(initialBadLevelIndex+1);
+
+                    bool removalList0IsSafe = LevelsAreSafe(removalList0, out int ignore0);
+                    bool removalList1IsSafe = LevelsAreSafe(removalList1, out int ignore1);
+                    bool removalList2IsSafe = LevelsAreSafe(removalList2, out int ignore2);
+
+                    listIsSafe = removalList0IsSafe || removalList1IsSafe || removalList2IsSafe;
+
+                    if (!listIsSafe)
+                    {
+                        int z = 0;
+                    }
+                }
+                
+                if (listIsSafe)
+                {
+                    safeCount++;
+                }
             }
             
-            Console.WriteLine($"Similarity sum: {similaritySum}");
+            Console.WriteLine($"Safe Count: {safeCount}");
 
+            bool LevelsAreSafe(List<int> levelList, out int initialBadLevelIndex)
+            {
+                bool levelsAreSafe = true;
+                initialBadLevelIndex = -1;
+                
+                bool isIncreasing = false;
+                bool isDecreasing = false;
+                
+                for (int j = 0; j < levelList.Count - 1; j++)
+                {
+                    int thisElement = levelList[j];
+                    int nextElement = levelList[j + 1];
+                    int difference = nextElement - thisElement;
+
+                    if (!isIncreasing && !isDecreasing)
+                    {
+                        if (difference > 0 && difference <= 3)
+                        {
+                            isIncreasing = true;
+                        }
+                        else if (difference < 0 && difference >= -3)
+                        {
+                            isDecreasing = true;
+                        }
+                        else
+                        {
+                            levelsAreSafe = false;
+                            initialBadLevelIndex = j;
+                            break;
+                        }
+                    }
+                    else if (isIncreasing && (difference <= 0 || difference > 3))
+                    {
+                        levelsAreSafe = false;
+                        initialBadLevelIndex = j;
+                        break;
+                    }
+                    else if (isDecreasing && (difference >= 0 || difference < -3))
+                    {
+                        levelsAreSafe = false;
+                        initialBadLevelIndex = j;
+                        break;
+                    }
+                }
+
+                return levelsAreSafe;
+            }
         }
     }
 }
